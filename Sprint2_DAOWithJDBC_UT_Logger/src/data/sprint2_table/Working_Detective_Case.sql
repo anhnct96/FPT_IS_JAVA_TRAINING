a3600a0659b18c72b8f5e500f910c9864f71501a
anhnct96 <105636140+anhnct96@users.noreply.github.com>
2022-06-13 21:11:22 +0700

    Update Working_Detective_Case.sql

--- Sprint2_DAOWithJDBC_UT_Logger/src/data/sprint2_table/Working_Detective_Case.sql
+++ Sprint2_DAOWithJDBC_UT_Logger/src/data/sprint2_table/Working_Detective_Case.sql
@@ -1,4 +1,9 @@
 -- 3rd
 CREATE TABLE Working_Detective_Case(
-
+workingId INT,
+detectiveId INT,
+criminalCaseId INT,
+CONSTRAINT pk_te PRIMARY KEY (id),
+CONSTRAINT detective_fk FOREIGN KEY detectiveId REFERENCES Detective(id),
+CONSTRAINT case_fk FOREIGN KEY criminalCaseId REFERENCES CriminalCase(id),
 )
