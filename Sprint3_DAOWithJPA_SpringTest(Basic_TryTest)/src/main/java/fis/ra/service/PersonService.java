cbeae5f8e567063ddc8ead1985ea55b1d118fcab
anhnct96 <105636140+anhnct96@users.noreply.github.com>
2022-06-21 01:33:11 +0700

    upload
    sprint 3 - v1.0

---
+++ Sprint3_DAOWithJPA_SpringTest(Basic_TryTest)/src/main/java/fis/ra/service/PersonService.java
@@ -0,0 +1,18 @@
+//package fis.ra.service;
+//
+//import fis.ra.repository.PersonRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Set;
+//
+//@Service
+//public class PersonService {
+//    @Autowired
+//    private PersonRepo jdbcPersonRepo;
+//
+//    public void testPersonRepo(){
+//        Set<Person> personSet = jdbcPersonRepo.findAll();
+//        System.out.println(personSet);
+//    }
+//}
